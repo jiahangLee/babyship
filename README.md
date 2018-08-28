2aa73827a82e58db520aadc976ce4bf53d951464
jiahanglee <1191039404@qq.com>
2018-08-28 17:59:17 +0800

    简单构建

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # babyship
 baochunkuajing
+
+端口服务命令
+
+- netstat -ntlp
+- ps -ef|grep java
+- application.yml文件替代proprietary文件
+* 热加载、热部署、热启动
--- src/main/java/com/jiahanglee/babyship/controller/HelloWorld.java
+++ src/main/java/com/jiahanglee/babyship/controller/HelloWorld.java
@@ -3,10 +3,27 @@ package com.jiahanglee.babyship.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author jiahanglee
+ * @version 0.0.1
+ * @date 2018/8/28 15:31
+ * @description //TODO
+ */
 @RestController
 public class HelloWorld {
+   /**
+    * @author jiahanglee
+    * @description //TODO
+    * @date 15:30 2018/8/28
+    * @Param [sddd]
+    * @return java.lang.String
+    **/
     @RequestMapping("/hello")
-    public static String test(){
-        return "hello world!!!5555ere55";
+    public static String test(String sddd){
+        return "hello world!!!5555ere55erewr4545454ddfdffdfdfdytytyty";
     }
+    public static String test2(String jiji){
+        return "zhehsiwo";
+    }
+
 }

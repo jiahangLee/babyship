42373375a6f41721eb322b028a913d43e6b6286a
jiahanglee <1191039404@qq.com>
2018-09-10 16:26:21 +0800

    。。。

---
+++ src/main/java/com/jiahanglee/babyship/controller/ToWeb.java
@@ -0,0 +1,29 @@
+package com.jiahanglee.babyship.controller;
+
+/**
+ * code is far away from bug with the animal protecting
+ * ┏┓　　　┏┓
+ * ┏┛┻━━━┛┻┓
+ * ┃　　　　　　　┃
+ * ┃　　　━　　　┃
+ * ┃　┳┛　┗┳　┃
+ * ┃　　　　　　　┃
+ * ┃　　　┻　　　┃
+ * ┃　　　　　　　┃
+ * ┗━┓　　　┏━┛
+ * 　　┃　　　┃神兽保佑
+ * 　　┃　　　┃代码无BUG！
+ * 　　┃　　　┗━━━┓
+ * 　　┃　　　　　　　┣┓
+ * 　　┃　　　　　　　┏┛
+ * 　　┗┓┓┏━┳┓┏┛
+ * 　　　┃┫┫　┃┫┫
+ * 　　　┗┻┛　┗┻┛
+ *
+ * @author jiahanglee
+ * @version 0.0.1
+ * @date 2018/9/10 16:16
+ * @description //TODO
+ */
+public class ToWeb {
+}

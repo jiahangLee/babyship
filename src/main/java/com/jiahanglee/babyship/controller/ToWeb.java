36dc300b61136c0f2db4934e0c2e84e05317feb3
jiahanglee <1191039404@qq.com>
2018-09-10 23:57:43 +0800

    a little

--- src/main/java/com/jiahanglee/babyship/controller/ToWeb.java
+++ src/main/java/com/jiahanglee/babyship/controller/ToWeb.java
@@ -1,5 +1,16 @@
 package com.jiahanglee.babyship.controller;
 
+import com.github.pagehelper.PageInfo;
+import com.jiahanglee.babyship.entity.News;
+import com.jiahanglee.babyship.entity.Teacher;
+import com.jiahanglee.babyship.service.NewsService;
+import com.jiahanglee.babyship.service.TeacherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * code is far away from bug with the animal protecting
  * ┏┓　　　┏┓
@@ -25,5 +36,32 @@ package com.jiahanglee.babyship.controller;
  * @date 2018/9/10 16:16
  * @description //TODO
  */
+@CrossOrigin
+@RestController
 public class ToWeb {
+
+    @Autowired
+    private NewsService newsService;
+    @Autowired
+    private TeacherService teacherService;
+
+    @GetMapping(value = "/getNews")
+    public PageInfo<News> getNews(
+        @RequestParam(name = "pageNum", required = false, defaultValue = "1")
+        int pageNum,
+        @RequestParam(name = "pageSize", required = false, defaultValue = "10")
+        int pageSize) {
+            return newsService.findAllTeacher(pageNum, pageSize);
+    }
+
+    @GetMapping(value = "/getTeachers")
+    public PageInfo<Teacher> getTeachers(
+            @RequestParam(name = "pageNum", required = false, defaultValue = "1")
+                    int pageNum,
+            @RequestParam(name = "pageSize", required = false, defaultValue = "10")
+                    int pageSize) {
+        return teacherService.findAllTeacher(pageNum, pageSize);
+    }
+
+
 }
